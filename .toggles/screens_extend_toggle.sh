dc6f03ce5bfb0453bb519756e082b64541ab4268
Navdeep Gaur <sendyourmailformehere@gmail.com>
2020-08-09 00:28:27 +0530

    Added a call to feh in screen toggler, rofi keyboard shortcut
    - called feh to pick random wallpapers from ~/Pictures/
    - if installed rofi will fire up with Win+t now!
    Changes to be committed:
    
    modified:   .config/i3/config
    modified:   .toggles/screens_extend_toggle.sh

--- .toggles/screens_extend_toggle.sh
+++ .toggles/screens_extend_toggle.sh
@@ -8,5 +8,9 @@ if [ ! -e $TOGGLE ]; then
     xrandr --output VGA-0 --off --output eDP --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-0 --off
 else
     [ -f $TOGGLE ] && rm $TOGGLE
-    xrandr --output VGA-0 --off --output eDP --primary --mode 1366x768 --pos 1920x440 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal
+    xrandr --output VGA-0 --off --output eDP --primary --mode 1366x768 --pos 0x440 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 1368x0 --rotate normal
 fi
+
+
+feh --randomize --bg-fill ~/Pictures/walls/*
+
